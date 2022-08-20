04b05301a614bd2bb13a3914b1c3f16ecc548770
Hend123 <henda4544@gmail.com>
2022-08-20 01:14:03 -0700

    Initial commit

---
+++ app/src/main/java/com/hend/customview1_fancontroller/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hend.customview1_fancontroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
